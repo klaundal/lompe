3c6fb2e1481b480bfcf380b33291c8402eb36ded
Fasil Kebede <46403603+FasilGibdaw@users.noreply.github.com>
2024-06-28 15:20:47 +0200

    sussi data downloader from jhuapl or cdaweb

---
+++ lompe/data_tools/datadownloader.py
@@ -0,0 +1,108 @@
+import numpy as np
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import os
+
+
+def download_sussi(event, destination='downloads', source='jhuapl'):
+    """function to download SSUSI data for a given event date
+    Example usage:
+        event = '2014-08-20'
+        detination = 'downloads'
+        source = 'cdaweb'
+        download_sussi(event, destination, source)
+
+    Args:
+        event (str): format YYYY-MM-DD
+        destination (str, optional): where to save the data. Defaults to 'downloads'.
+        source (str, optional): Defaults to 'jhuapl'. cedaweb is the other option.
+
+    Note: I (Fasil) prefer the cdaweb because it is more faster to downlaod.
+          but the read_sussi function in lompe package is tailored to the jhuapl data.
+    """
+
+    year = int(event[0:4])
+    doy = date2doy(event)
+    os.makedirs(destination, exist_ok=True)
+    # iterate over the satellites with SSUSI data
+    for sat in [16, 17, 18, 19]:
+        if source == 'jhuapl':
+            url = f"https://ssusi.jhuapl.edu/data_retriver?spc=f{sat}&type=edr-aur&year={year}&Doy={doy}"
+        elif source == 'cdaweb':
+            url = f'https://cdaweb.gsfc.nasa.gov/pub/data/dmsp/dmspf{sat}/ssusi/data/edr-aurora/{year}/{doy}/'
+        else:
+            print(f"Unsupported source: {source}")
+            continue
+        # content of the webpage
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for link in soup.find_all('a', href=True):
+            href = link.get('href')
+            if href.lower().endswith('.nc'):  # looking for .NC (jhuapl) and .nc (cdaweb) files
+                file_url = urljoin(url, href)
+                try:
+                    # Download the file
+                    # probably too much print commented out the print statement if needed
+                    print(f"Downloading {file_url}...")
+                    response = requests.get(file_url, stream=True)
+                    response.raise_for_status()  # raise an HTTPError on bad response
+                    filename = os.path.join(
+                        destination, os.path.basename(file_url))
+
+                    with open(filename, 'wb') as file:
+                        # 64KB chuck size
+                        for chunk in response.iter_content(chunk_size=65536):
+                            if chunk:  # Filter out keep-alive new chunks
+                                file.write(chunk)
+                except requests.exceptions.RequestException as e:
+                    print(f"Failed to download {file_url}: {e}")
+
+    print("Download complete!")
+    return None
+
+
+def download_smag():
+    pass
+
+
+def download_iridium():
+    pass
+
+
+def download_supermag():
+    pass
+
+
+def download_dmsp():
+    pass
+
+
+def download_swarm():
+    pass
+
+
+def date2doy(event):
+    ''' function to compute day of the year given a date string in YYYY-MM-DD format '''
+
+    year, month, day = map(int, event.split('-'))
+
+    # days of each month
+    daysmonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    # check leap year and adjust February days
+    if ((year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)):
+        daysmonth[1] += 1
+
+    # day of the year, doy
+    if month > 1:
+        doy = np.sum(daysmonth[:month-1]) + day
+    else:
+        doy = day
+
+    return int(doy)
+
+
+if __name__ == '__main__':
+    print("This is a module to download SSUSI data for a given event date.")
